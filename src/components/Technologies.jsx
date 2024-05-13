4d2da0d00b56021582a5801db81242a1dd0fade4
vitatriutami <vitatriutami@gmail.com>
2024-05-14 03:19:43 +0700

    adding framer motion to expe section

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -24,7 +24,7 @@ const Technologies = () => {
     <div className="border-b border-neutral-800 pb-24">
       <motion.h1
         whileInView={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, x: -100 }}
+        initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 1.5 }}
         className="my-20 text-center text-4xl"
       >
