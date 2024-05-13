5816e8a689ff60ea018d3ac517472befc2ed35b3
vitatriutami <vitatriutami@gmail.com>
2024-05-14 03:03:06 +0700

    adding framer motion to hero section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className="border-b border-neutral-900 pb-4">
       <h1 className="my-20 text-center text-4xl">
         About
-        <span className="text-neutral-500">Me</span>
+        <span className="text-neutral-500"> Me</span>
       </h1>
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2 lg:p-8">
