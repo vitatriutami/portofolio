a5d19d45eed7eae4c9242745f1fe1fb7368d742f
vitatriutami <vitatriutami@gmail.com>
2024-05-14 07:02:05 +0700

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import logo from "../assets/kevinRushLogo.png"
-import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa"
-import { FaSquareXTwitter } from "react-icons/fa6"
-
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo" />
+        <h1 className="text-4xl font-bold bg-gradient-to-r from-slate-300 via-slate-500 to-slate-500 bg-clip-text tracking-tight text-transparent">
+          vita.
+        </h1>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a
@@ -15,14 +16,24 @@ const Navbar = () => {
           target="_black"
           rel="noopener"
         >
-          <FaLinkedin />
+          <FaLinkedin className="hover:-translate-y-0.5 transform transition hover:bg-cyan-700" />
         </a>
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <a
+          href="https://github.com/vitatriutami"
+          target="_black"
+          rel="noopener"
+        >
+          <FaGithub className="hover:-translate-y-0.5 transform transition hover:bg-cyan-700" />
+        </a>
+        <a href="https://instagram.com/vita__tr" target="_black" rel="noopener">
+          <FaInstagram className="hover:-translate-y-0.5 transform transition hover:bg-cyan-700" />
+        </a>
+        <a href="https://x.com/" target="_black" rel="noopener">
+          <FaSquareXTwitter className="hover:-translate-y-0.5 transform transition hover:bg-cyan-700" />
+        </a>
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/constants/index.js
+++ src/constants/index.js
@@ -10,7 +10,7 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 export const EXPERIENCES = [
   {
     year: "March 2023 - Present",
-    role: "Full Stack Web Developer",
+    role: "Full Stack Web Development",
     company: "Devscale Indonesia",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
@@ -24,7 +24,7 @@ export const EXPERIENCES = [
   },
   {
     year: "Aug 2022 - Apr 2023",
-    role: "Full Stack Web Developer",
+    role: "Full Stack Web Development",
     company: "Binar Academy",
     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
